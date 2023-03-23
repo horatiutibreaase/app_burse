fdd1ae41872311e21f8656eab3485105bb2f165a
alxxalex <105727854+alxxalex@users.noreply.github.com>
2023-03-23 15:27:04 +0200

    backend

---
+++ app_burse_demo/app_burse/app_burse/doc/bd_old/Rom_str.sql
@@ -0,0 +1,5 @@
+ DROP TABLE IF EXISTS Rom_str;
+ CREATE TABLE Rom_str (
+TIP_TARA                         Char(10));
+insert into Rom_str values('Român');
+insert into Rom_str values('Strãin');
---
+++ app_burse_demo/app_burse/app_burse/doc/bd_old/Tip_stud.sql
@@ -0,0 +1,8 @@
+ DROP TABLE IF EXISTS tip_stud;
+ CREATE TABLE tip_stud (
+TIP_STUD                         Char(10));
+insert into tip_stud values('Student ID');
+insert into tip_stud values('Student ZI');
+insert into tip_stud values('Masterand');
+insert into tip_stud values('Doctorand');
+insert into tip_stud values('');
