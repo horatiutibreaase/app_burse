b15f5d101033bc72f76953dcade4246b9eb8bcf8
Oprea Vlad <126592776+r1gzy@users.noreply.github.com>
2023-03-27 22:07:38 +0300

    Update BD_Facultate.cs

--- app_burse_back/backend_1/Models/BD_Facultate.cs
+++ app_burse_back/backend_1/Models/BD_Facultate.cs
@@ -7,7 +7,7 @@ namespace backend_1.Models
         [Key]
         public int id_facultate { get; set; }
 
-        [StringLength(70,ErrorMessage ="Limita de 70 de caractere")]//lungime maxima de 70 caractere pt ca in baza de date e varchar2(70)
+        [StringLength(70,ErrorMessage ="Limita de 70 de caractere")]//lungime maxima de 70 caractere pt ca in baza de date e varchar(70)
         public string? denumire_facultate { get; set; }
     }
 }
